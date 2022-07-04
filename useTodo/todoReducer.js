db79fc700251f2f6bd2048f3e21c65d348755416
Karol Olg Hdz <karolhe@gmail.com>
2022-07-03 21:46:42 -0500

    Agregado el todoReducer

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,26 @@
+
+
+export const todoReducer = (initialState = [], action) =>{
+
+    switch (action.type) {
+        case 'ADD [TODO]':
+            return [...initialState, action.payload];
+            
+        case 'DELETE [TODO]':
+            return initialState.filter( todo => todo.id !== action.payload ); 
+
+        case 'TOGGLE [TODO]':
+            return initialState.map( todo => {
+                if( todo.id === action.payload ) {
+                    return{
+                        ...todo, 
+                        done: !todo.done
+                    }
+                }
+                return todo;
+            });
+    
+        default:
+            return initialState;
+    }
+} 
